e6bbdb67839019822f9205cffa8a3e881fcf7da0
James Embrey <james.alexander.embrey@gmail.com>
2016-07-17 17:27:10 -0700

    Update InHouseOrcGen.py

--- InHouseOrcGen.py
+++ InHouseOrcGen.py
@@ -4,3 +4,9 @@
 # atleast 25% points spent on Core
 # no more than 50% points spent on Special, no more than 3 of the same unit
 # no more than 25% points spend on Rare, no more than 2 of the same unit
+Core = ["Orc Boyz", "Orc Arrer Boyz", "Savage Orc Boyz", "Big 'Uns", "Goblin Wolf Riders", "Goblins", "Night Goblins", "Forest Goblin Spider Riders"]
+Special = ["Black Orcs", "Orc Boar Boyz", "Goblin Wolf Chariot", "Orc Boar Chariot", "Trolls"]
+Rare = ["Giant", "Rock Lobba", "Doom Diver", "Arachnarok"]
+Lord = ["Grimgor Ironhide", "Azhag the Slaughterer", "Orc Warboss", "Great Goblin Shaman"]
+Hero = ["Goblin Boss", "Night Goblin Shaman", "Orc Shaman"]
+Points = input("What is your starting gold?")
