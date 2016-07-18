6ea1a7dc916317b682744e2e4d5a6fb7593852e8
James Embrey <james.alexander.embrey@gmail.com>
2016-07-17 17:06:22 -0700

    Create InHouseOrcGen.py

---
+++ InHouseOrcGen.py
@@ -0,0 +1,6 @@
+#auto generation for Total Warhammer Orcs & Goblins Army following the following rules
+# no more than 25% of points on Lord, But atleast 1 lord
+# no more than 25% of points on Hero
+# atleast 25% points spent on Core
+# no more than 50% points spent on Special, no more than 3 of the same unit
+# no more than 25% points spend on Rare, no more than 2 of the same unit
